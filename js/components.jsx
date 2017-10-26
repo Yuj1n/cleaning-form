70d6d43f17979b3d4a343d002dcdc875a50d11c1
Jordan Holland <thisrememo@gmail.com>
2017-10-26 15:25:21 +1000

    Changed .gitignore.

---
+++ js/components.jsx
@@ -0,0 +1,343 @@
+const BOND_CLEAN = "6";
+
+const FREQUENCY_VALUES = {
+  1: "One Off Service",
+  4: "Weekly",
+  3: "Fortnightly",
+  5: "Every 3 Weeks",
+  2: "Monthly",
+  6: "Bond Clean/Move In Move Out"
+};
+
+const BEDROOM_VALUES = {
+  12: 1,
+  2: 2,
+  8: 3,
+  9: 4,
+  5: 5,
+  6: 6
+};
+
+class Container extends React.Component {
+  render() {
+    return (
+      <div id="container" style={this.props.style}>
+        <CleaningForm {...this.props} />
+      </div>
+    );
+  }
+}
+
+class CleaningForm extends React.Component {
+  static defaultProps = {
+    option: "b"
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      email: "",
+      phoneNumber: "",
+      bedrooms: "12",
+      bathrooms: "1",
+      frequency: "3",
+      warnings: []
+    };
+  }
+
+  onNameChange = event => {
+    this.setState({
+      firstName: event.target.value
+    });
+  };
+
+  onEmailChange = event => {
+    this.setState({
+      email: event.target.value
+    });
+  };
+
+  onNumberChange = event => {
+    this.setState({
+      phoneNumber: event.target.value
+    });
+  };
+
+  onBedroomsChange = event => {
+    this.setState({
+      bedrooms: event.target.value
+    });
+  };
+
+  onBathroomsChange = event => {
+    this.setState({
+      bathrooms: event.target.value
+    });
+  };
+
+  onFrequencyChange = event => {
+    this.setState({
+      frequency: event.target.value
+    });
+  };
+
+  // Handles a submit event(enter key, button click etc.)
+  handleSubmit = event => {
+    this.onBookClick();
+    event.preventDefault();
+  };
+
+  // Triggers when the form is submitted
+  onBookClick = () => {
+    if (!this.inputsComplete()) return;
+    fetch(
+      "https://script.google.com/macros/s/AKfycbwVLBJyzQmJexJsKopeMiy9athlJieCYgeRVOPJrHJmBNzLx7X4/exec",
+      {
+        method: "post",
+        body: JSON.stringify({
+          firstName: this.state.firstName,
+          email: this.state.email,
+          phoneNumber: this.state.phoneNumber,
+          frequency: FREQUENCY_VALUES[this.state.frequency],
+          bedrooms: BEDROOM_VALUES[this.state.bedrooms],
+          bathrooms: this.state.bathrooms,
+          source: window.location.href
+        })
+      }
+    ).then(response => {
+      var newLocation = this.createRedirectLocation();
+      newLocation = this.applyDiscountCode(newLocation);
+      window.location.href = newLocation;
+    });
+  };
+
+  // Sanitizes the inputs, adds warnings to state if they are incomplete
+  inputsComplete = () => {
+    let warnings = [];
+
+    if (this.state.firstName === "") {
+      warnings.push("firstName");
+    }
+
+    // TODO: Add validation for phone number
+    if (this.state.phoneNumber === "") {
+      warnings.push("phoneNumber");
+    }
+
+    if (this.state.email === "" || !this.validateEmail(this.state.email)) {
+      warnings.push("email");
+    }
+
+    if (warnings.length > 0) {
+      this.setState({
+        warnings: warnings
+      });
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  // Creates a URL to redirect to based on parameters
+  // TODO: Potentially change to HTTPS URL
+  createRedirectLocation = () => {
+    if (this.state.frequency === BOND_CLEAN) {
+      return `http://snapclean.com.au/booking-page/?first_name=${this.state.firstName}&email=${this.state.email}&phone=${this.state.phoneNumber}&service_id=${this.state.bedrooms}&pricing_parameter[1]=${this.state.bathrooms}&frequency_id=1&extra[20]=1`;
+    } else {
+      return `http://snapclean.com.au/booking-page/?first_name=${this.state.firstName}&email=${this.state.email}&phone=${this.state.phoneNumber}&service_id=${this.state.bedrooms}&pricing_parameter[1]=${this.state.bathrooms}&frequency_id=${this.state.frequency}`;
+    }
+  };
+
+  // Applies a discount code to the new URL if it has been specified
+  applyDiscountCode = redirectLocation => {
+    return this.props.discountCode
+      ? `${redirectLocation}&rcode=${this.props.discountCode}`
+      : redirectLocation;
+  };
+
+  // Validates an email address
+  validateEmail(email) {
+    var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email);
+  }
+
+  // Validates a phone number(simple)
+  validatePhoneNumber(number) {
+    var regex = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
+    return regex.test(number);
+  }
+
+  // Gets the style for inputs, so it will show a red line if they are un-filled
+  getInputStyle = name => {
+    if (this.state.warnings.indexOf(name) > -1) {
+      return {
+        borderWidth: 2,
+        borderColor: "red"
+      };
+    } else {
+      return {
+        border: "none"
+      };
+    }
+  };
+
+  // Renders option A (1x1)
+  renderOptionA = () => {
+    return (
+      <div className="selectionContainer" id="selectionContainer1">
+        {this.renderNameInput()}
+        {this.renderEmailInput()}
+        {this.renderPhoneNumberInput()}
+        {this.renderBedroomSelection()}
+        {this.renderBathroomSelection()}
+        {this.renderFrequencySelection()}
+      </div>
+    );
+  };
+
+  // Renders option B (3x2)
+  renderOptionB = () => {
+    return (
+      <div className="selectionContainer" id="selectionContainer2">
+        <div className="selectionRow">
+          {this.renderNameInput()}
+          {this.renderEmailInput()}
+          {this.renderPhoneNumberInput()}
+        </div>
+        <div className="selectionRow">
+          {this.renderBedroomSelection()}
+          {this.renderBathroomSelection()}
+          {this.renderFrequencySelection()}
+        </div>
+      </div>
+    );
+  };
+
+  // Renders option C (2x3)
+  renderOptionC = () => {
+    return (
+      <div className="selectionContainer" id="selectionContainer2">
+        <div className="selectionRow">
+          {this.renderNameInput()}
+          {this.renderPhoneNumberInput()}
+        </div>
+        <div className="selectionRow">
+          {this.renderEmailInput()}
+          {this.renderFrequencySelection()}
+        </div>
+        <div className="selectionRow">
+          {this.renderBedroomSelection()}
+          {this.renderBathroomSelection()}
+        </div>
+      </div>
+    );
+  };
+
+  renderNameInput = () => {
+    return (
+      <input
+        type="text"
+        className="textInput"
+        style={this.getInputStyle("firstName")}
+        value={this.state.firstName}
+        onChange={this.onNameChange}
+        placeholder="First Name"
+      />
+    );
+  };
+
+  renderPhoneNumberInput = () => {
+    return (
+      <input
+        type="text"
+        className="textInput"
+        style={this.getInputStyle("phoneNumber")}
+        value={this.state.phoneNumber}
+        onChange={this.onNumberChange}
+        placeholder="Phone Number"
+      />
+    );
+  };
+
+  renderEmailInput = () => {
+    return (
+      <input
+        type="text"
+        className="textInput"
+        style={this.getInputStyle("email")}
+        value={this.state.email}
+        onChange={this.onEmailChange}
+        placeholder="Email"
+      />
+    );
+  };
+
+  renderBedroomSelection = () => {
+    return (
+      <select value={this.state.bedrooms} onChange={this.onBedroomsChange}>
+        <option value="12">1 Bedroom</option>
+        <option value="2">2 Bedrooms</option>
+        <option value="8">3 Bedrooms</option>
+        <option value="9">4 Bedrooms</option>
+        <option value="5">5 Bedrooms</option>
+        <option value="6">6 Bedrooms</option>
+      </select>
+    );
+  };
+
+  renderBathroomSelection = () => {
+    return (
+      <select value={this.state.bathrooms} onChange={this.onBathroomsChange}>
+        <option value="1">1 Bathroom</option>
+        <option value="2">2 Bathrooms</option>
+        <option value="3">3 Bathrooms</option>
+        <option value="4">4 Bathrooms</option>
+        <option value="5">5 Bathrooms</option>
+        <option value="6">6 Bathrooms</option>
+      </select>
+    );
+  };
+
+  renderFrequencySelection = () => {
+    return (
+      <select value={this.state.frequency} onChange={this.onFrequencyChange}>
+        <option value="3">Fortnightly</option>
+        <option value="4">Weekly</option>
+        <option value="5">Every 3 Weeks</option>
+        <option value="2">Monthly</option>
+        <option value="1">One Off Service</option>
+        <option value="6">Move In Move Out</option>
+      </select>
+    );
+  };
+
+  // Renders the inputs and selects depending on the option specified
+  renderSelectionItems = () => {
+    if (this.props.option === "a") {
+      return this.renderOptionA();
+    } else if (this.props.option === "b") {
+      return this.renderOptionB();
+    } else if (this.props.option === "c") {
+      return this.renderOptionC();
+    }
+  };
+
+  render() {
+    return (
+      <div id="formContainer">
+        <h1 id="formTitle">Get an online quote in seconds</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderSelectionItems()}
+          <div id="submitContainer">
+            <input
+              id="submitButton"
+              type="submit"
+              value="VIEW YOUR PRICE ONLINE NOW"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
