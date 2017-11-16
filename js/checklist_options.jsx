55d8e8859ce28a6686648f0437e17de36829afa9
Jordan Holland <thisrememo@gmail.com>
2017-11-16 10:16:20 +1000

    Added styling for video.

---
+++ js/checklist_options.jsx
@@ -0,0 +1,176 @@
+var checkListPages = {
+  ALWAYS_INCLUDED: {
+    title: "Always Included",
+    items: [
+      {
+        name: "$5,000,000 of public liability insurance to keep you and your belongings safe",
+        types: ["regular", "deep", "move"]
+      },
+      {
+        name: "Unparalleled customer service by email, live-chat and telephone",
+        types: ["regular", "deep", "move"]
+      },
+      {
+        name: "Great discounts for regular cleaning",
+        types: ["regular", "deep", "move"]
+      },
+      {
+        name: "SnapClean satisfaction guarantee",
+        types: ["regular", "deep", "move"]
+      }
+    ]
+  },
+  ALL_AREAS: {
+    title: "All Areas",
+    items: [
+      {
+        name: "Dusting",
+        types: ["regular", "deep", "move"]
+      },
+      {
+        name: "Empty Rubbish Bins",
+        types: ["regular", "deep", "move"]
+      },
+      {
+        name: "Vaccum Carpets & Rugs",
+        types: ["regular", "deep", "move"]
+      },
+      {
+        name: "Mop Floors",
+        types: ["regular", "deep", "move"]
+      },
+      {
+        name: "Clean Mirrors",
+        types: ["regular", "deep", "move"]
+      },
+      {
+        name: "Wipe Door Handles & Light Switches",
+        types: ["regular", "deep", "move"]
+      },
+      {
+        name: "Dust Windows, Sills and Ledges",
+        types: ["regular", "deep", "move"]
+      }
+    ]
+  },
+  KITCHEN: {
+    title: "Kitchen",
+    items: [
+      {
+        name: "Wipe Stove Top & Stove Fan",
+        types: ["regular", "deep", "move"]
+      },
+      {
+        name: "Wipe Benchtops",
+        types: ["regular", "deep", "move"]
+      },
+      {
+        name: "Clean Microwave(In & Out)",
+        types: ["regular", "deep", "move"]
+      },
+      {
+        name: "Wipe Dishwasher",
+        types: ["regular", "deep", "move"]
+      },
+      {
+        name: "Wipe Fridge(Exterior)",
+        types: ["regular", "deep", "move"]
+      },
+      {
+        name: "Wipe Small Appliances",
+        types: ["regular", "deep", "move"]
+      },
+      {
+        name: "Wipe Stools & Chairs",
+        types: ["regular", "deep", "move"]
+      }
+    ]
+  },
+  BATHROOMS: {
+    title: "Bathrooms",
+    items: [
+      {
+        name: "Wipe Bathroom Cabinets(Exterior)",
+        types: ["regular", "deep", "move"]
+      },
+      {
+        name: "Scrub & Clean Sinks",
+        types: ["regular", "deep", "move"]
+      },
+      {
+        name: "Scrub & Clean Toilets",
+        types: ["regular", "deep", "move"]
+      },
+      {
+        name: "Scrub & Clean Bath Tubs",
+        types: ["regular", "deep", "move"]
+      },
+      {
+        name: "Scrub & Clean Showers",
+        types: ["regular", "deep", "move"]
+      }
+    ]
+  },
+  BEDROOMS: {
+    title: "Bedrooms",
+    items: [
+      {
+        name: "Make Bed",
+        types: ["regular", "deep", "move"]
+      },
+      {
+        name: "Dust Furniture & Decorations",
+        types: ["regular", "deep", "move"]
+      }
+    ]
+  },
+  OTHER: {
+    title: "Other",
+    items: [
+      {
+        name: "Wipe Baseboards",
+        types: ["deep", "move"]
+      },
+      {
+        name: "Extra Attention to Outside of Kitchen Appliances",
+        types: ["deep", "move"]
+      },
+      {
+        name: "Extra Attention to Shower & Bathroom",
+        types: ["deep", "move"]
+      },
+      {
+        name: "Interior Cabinet Cleaning",
+        types: ["move"]
+      },
+      {
+        name: "Inside Fridge",
+        types: ["move"]
+      },
+      {
+        name: "Inside Oven",
+        types: ["move"]
+      },
+      {
+        name: "Inside Windows",
+        types: ["move"]
+      },
+      {
+        name: "Walls",
+        types: ["move"]
+      },
+      {
+        name: "Carpets Cleaning",
+        types: []
+      },
+      {
+        name: "Pest Control",
+        types: []
+      },
+      {
+        name: "Blinds",
+        types: []
+      }
+    ]
+  }
+};
